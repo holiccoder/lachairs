8b5302d83fa92614d8d34d22384d66e1c61e69fe
Tycoon INC <sachu@vip.qq.com>
2026-07-10 00:12:07 +0800

    chore: update image remote pattern and add scraping devDependencies

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "picsum.photos",
-      },
-      {
-        protocol: "https",
-        hostname: "lachairs.com",
+        hostname: "admin.lachairs.com",
       },
     ],
   },
